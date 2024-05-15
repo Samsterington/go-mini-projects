6e9006e9b3fe90e642c2f839c110b587b8bcfbdd
Sam <sam.williams@vivacitylabs.com>
2024-05-15 09:24:27 +0100

    [fix] root of repo

--- cmd/app/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "go-mini-projects/map-api/internal/key_value_store"
-
-func main() {
-	key_value_store.Create()
-}
---
+++ map-api/cmd/app/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"go-mini-projects/map-api/internal/app"
+)
+
+func main() {
+	app.SetupRoutes()
+}
